9be1974aa7a819a1a9bd89e52cfc393aa51e3b8a
Arnie97 <arnie97@gmail.com>
2020-07-10 21:34:56 +0800

    Fix adapter for CR Guangzhou

--- adapters/guangzhou.go
+++ adapters/guangzhou.go
@@ -1,9 +1,8 @@
 package adapters
 
 import (
-	"bytes"
-	"encoding/json"
 	"fmt"
+	"net/http"
 	"strings"
 
 	"github.com/arnie97/emu-log/common"
@@ -30,33 +29,29 @@ func (Guangzhou) BruteForce(serials chan<- string) {
 }
 
 func (b Guangzhou) Info(serial string) (info jsonObject, err error) {
-	const api = "https://v3i.minicart.cn/shopping/v3/getTrainnum"
-	values := jsonObject{
-		"qr_code": strings.TrimLeft(serial, "0"),
-		"mpid":    9,
-		"mp_id":   9,
-		"mid":     9,
-		"token":   common.Conf(b.Code()),
-	}
-
-	jsonBytes, err := json.Marshal(values)
+	const api = "https://sj-api.yishizongheng.com/shejian/api/train/getByQrcode?qrcode=%s"
+	url := fmt.Sprintf(api, strings.TrimLeft(serial, "0"))
+	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		return
 	}
-	buf := bytes.NewBuffer(jsonBytes)
-	resp, err := common.HTTPClient().Post(api, contentType, buf)
+	req.Header.Set("authorization", common.Conf(b.Code()))
+	resp, err := common.HTTPClient().Do(req)
 	if err != nil {
 		return
 	}
 	defer resp.Body.Close()
 
 	var result struct {
-		Status string `json:"error_code"`
-		Msg    string
+		Status int    `json:"code"`
+		Msg    string `json:"message"`
 		Data   jsonObject
 	}
 	err = parseResult(resp, &result)
 	info = result.Data
+	if info != nil {
+		err = nil
+	}
 	return
 }
 
@@ -75,7 +70,9 @@ func (b Guangzhou) VehicleNo(serial string) (vehicleNo string, err error) {
 	info, err = b.Info(serial)
 	if err == nil {
 		defer common.Catch(&err)
-		vehicleNo = fmt.Sprintf("CR%s+%s", info["carriage_num"], serial)
+		vehicleNo = fmt.Sprintf(
+			"CR%s-%.0f+%s", info["carriageNum"], info["id"], serial,
+		)
 	}
 	return
 }
