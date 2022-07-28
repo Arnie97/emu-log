385df5e62753297349554de2bf5e204ab151b8d7
Arnie97 <arnie97@gmail.com>
2022-07-28 18:48:06 +0800

    Remove configuration loader from json.go

--- common/json_test.go
+++ common/json_test.go
@@ -2,23 +2,9 @@ package common_test
 
 import (
 	"fmt"
-	"os"
 
 	"github.com/arnie97/emu-log/common"
 )
-
-func ExampleConf() {
-	file, err := os.Create(common.AppPath() + "/emu-log.json")
-	if err != nil {
-		fmt.Println(err)
-	}
-	file.Write([]byte(`{"a": "1234", "b": "5678"}`))
-	file.Close()
-
-	fmt.Println(common.Conf("a"), common.Conf("b"))
-	// Output:
-	// 1234 5678
-}
 
 func ExamplePrettyPrint() {
 	common.PrettyPrint(map[string]interface{}{
