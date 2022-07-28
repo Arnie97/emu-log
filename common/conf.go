08600cb8d817b848c5730391971c0a51cd6b791d
Arnie97 <arnie97@gmail.com>
2022-07-28 18:52:43 +0800

    Remove JSON specific functions from conf.go

--- common/conf.go
+++ common/conf.go
@@ -6,12 +6,7 @@ import (
 	"io/ioutil"
 	"os"
 	"path/filepath"
-	"reflect"
 	"sync"
-)
-
-const (
-	ContentType = "application/json"
 )
 
 var (
@@ -52,30 +47,4 @@ func AppPath() string {
 	path, err := os.Executable()
 	Must(err)
 	return filepath.Dir(path)
-}
-
-// PrettyPrint displays a nested structure in human readable JSON format.
-func PrettyPrint(obj interface{}) {
-	jsonBytes, err := json.MarshalIndent(obj, "", "    ")
-	Must(err)
-	fmt.Printf("%s\n", jsonBytes)
-}
-
-// GetField takes an arbitary structure, and uses reflection to retrieve the
-// field with specified name from it. Panics if the field does not exist.
-func GetField(object interface{}, fieldName string) interface{} {
-	reflectObject := reflect.Indirect(reflect.ValueOf(object))
-	return reflectObject.FieldByName(fieldName).Interface()
-}
-
-// StructDecode takes a source structure, and uses reflection to translate it to the
-// destination structure. Typically used to convert native Go structures to
-// map[string]interface{}, and vice versa.
-func StructDecode(src interface{}, dest interface{}) error {
-	if bytes, err := json.Marshal(src); err != nil {
-		return err
-	} else if err = json.Unmarshal(bytes, dest); err != nil {
-		return err
-	}
-	return nil
 }
